43cfdc43dec933dba866a327267351ed7b39b294
Malte Müns <m.muens@muensmedia.de>
2014-10-27 15:19:02 +0100

    [FIX] Update text impressum

--- content/impressum.php
+++ content/impressum.php
@@ -1,6 +1,6 @@
 <?php
 // Meta-Informationen
-$infos['seitentitel'] = 'Startseite';
+$infos['seitentitel'] = 'Impressum';
 $infos['description'] = 'Description';
 $infos['keywords'] = array('Keyword1', 'keyword2');
 
@@ -10,14 +10,14 @@ $content = '
     <p><strong>Angaben gemäß § 5 TMG:</strong></p>
     <p>MÜNSMEDIA GbR<br>
     Hegelstraße 28<br>
-    39104 Magdeburg</p>
+    39104 Magdeburg<br><br>
+    <b>Vertretern durch:</b> Nino Müns, Malte Müns</p>
 
     <p>Igor Lückel<br>
     Sickingenstraße 3<br>
     39108 Magdeburg</p>
 
-    <p>Kontakt:</p>
+    <p><strong>Kontakt:</strong></p>
     <p>Telefon: 0391-50549380<br>
     E-Mail: info@fuckupnight.de</p>
-    <p>Quelle: http://www.e-recht24.de</p>
 </div>';
