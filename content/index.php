d371e3aecb95bbfbbe7b59f48bf1b38ac41532b8
Igor Lückel <igor.lueckel@googlemail.com>
2014-11-04 19:54:34 +0100

    Einleitungssatz Startseite hinzugefügt

--- content/index.php
+++ content/index.php
@@ -6,5 +6,6 @@ $infos['keywords'] = array('Keyword1', 'keyword2');
 
 $content = '
 <div class="content-area">
-    
+    <p>Erfunden in Mexico City ist die FuckUp Night eine Gelegenheit über Dinge zu sprechen, die nicht in unsere Gesellschaft zu passen scheinen: <strong>Niederlagen, gescheiterte Existenzen, zerplatzte Träume &amp; Pleiten!</strong> Nur wer Erfolg hat ist begehrt, so scheint es. Aber es gibt keinen Erfolg ohne Scheitern! Nur wer aus seinen Fehlern lernt, kann erfolgreich werden.</p>
+    <p>Im Rahmen einer FuckUp Night stellen jeweils 3 bis 4 Unternehmer ihre persönlichen Bruchlandungen vor und bieten somit dem Publikum einen reichhaltigen Erfahrungssschatz.</p>
 </div>';
