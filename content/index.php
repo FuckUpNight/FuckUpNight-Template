e5deb03d49a473adcdd1e9051a6329b85d3e9afa
Igor Lückel <igor.lueckel@googlemail.com>
2014-11-04 19:53:53 +0100

    Startseiteninhalt in Unterseiten verpackt

--- content/index.php
+++ content/index.php
@@ -6,23 +6,5 @@ $infos['keywords'] = array('Keyword1', 'keyword2');
 
 $content = '
 <div class="content-area">
-    <h2></h2>
-    <p>Erfunden in Mexico City ist die FuckUp Night eine Gelegenheit über Dinge zu sprechen, die nicht in unsere Gesellschaft zu passen scheinen: <strong>Niederlagen, gescheiterte Existenzen, zerplatzte Träume &amp; Pleiten!</strong> Nur wer Erfolg hat ist begehrt, so scheint es. Aber es gibt keinen Erfolg ohne Scheitern! Nur wer aus seinen Fehlern lernt, kann erfolgreich werden.</p>
-    <p>Im Rahmen einer FuckUp Night stellen jeweils 3 bis 4 Unternehmer ihre persönlichen Bruchlandungen vor und bieten somit dem Publikum einen reichhaltigen Erfahrungssschatz.</p>
-    <p>Wir sind Teil eines weltweiten Lernprozesses, der dafür sensibilisieren soll, dass Scheitern ganz alltäglich ist. Dafür muss man sich nicht schämen.</p>
-    <p>Dabei ist das Prinzip denkbar einfach: 3 Vortragende, nur wenige Folien und jeweils maximal 10 Minuten Redezeit.</p>
-    <p>Bereits in 18 Ländern weltweit haben Fuckupreneurs Vorträge über Ihr berufliches oder unternehmerischeres Handeln gehalten. Nach Mexico, Paris, Melbourne, Stockholm nun auch in Deutschland! Du kannst FuckUp Nights auch in deine Stadt holen</p>
-
-    <h2>Was ist eine FuckUp Night</h2>
-    <p>Du wurdest mal so richtig gefickt?<br/>Beruflich oder in der Selbstständigkeit?<br/></p>
-    <p>FuckUp Nights sind eine monatliche Eventreihe, bei der die Leute erzählen, über die man normal nur negativ spricht: Gescheiterte Unternehmer, Selbstständige, gescheiterte Visionäre. Öffentlich spricht man lieber über Menschen die über Nacht zum Millionär wurden. Aber in Realität gehört das Scheitern zum Erfolg dazu. Beide sind untrennbar miteinander verbunden. Dabei erzählen die Referenten auf wenigen Folien auf lustige Art & Weise in maximal 10 Minuten ihre eigene Version der Geschichte und was sie daraus gelernt haben. FuckUps Nights laden dazu ein aus den Fehlern anderer zu lernen und neue Geschäftsideen aufzuschnappen.</p>
-    <h3>Wie läuft das ab?</h3>
-    <p>Bei jeder FuckUp Night, tragen 3 bis 4 Fuckupreneurs vor und teilen Ihre Geschichten des Scheiterns und beantworten dabei die wichtigsten Fragen:</p>
-    <ul>
-        <li>Worum ging es bei der Idee?</li>
-        <li>Was ist schief gelaufen?</li>
-        <li>Was haben die daraus gelernt?</li>
-        <li>Was würden sie nun anders machen?</li>
-    </ul>
-    <p>Im Anschluss hat das Publikum die Möglichkeit Fragen zu stellen. Begleitet wird das Ganze durch eine angenehme Athomsphäre mit Snacks und Getränken.</p>
+    
 </div>';
