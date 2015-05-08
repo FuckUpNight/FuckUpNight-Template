649270c164d3b9e9914a97571f7cdad537758c1f
Igor Lückel <igor.lueckel@googlemail.com>
2015-05-08 11:16:09 +0200

    Open ThinkTank Link in a new tab

--- content/index.php
+++ content/index.php
@@ -20,7 +20,7 @@ $content = '
                                 </div>
                                 <div style="margin-top: 1.5rem">
                                     <span class="termin">28.05.2015</span><br>
-                                    <span class="ort"><a href="http://www.mdkk.de/de/kongresse-events/thinktank-kreativwirtschaft.html">@ThinkTank Kreativwirtschaft</a>, Magdeburg</span>
+                                    <span class="ort"><a href="http://www.mdkk.de/de/kongresse-events/thinktank-kreativwirtschaft.html" target="_blank">@ThinkTank Kreativwirtschaft</a>, Magdeburg</span>
                                 </div>
                             </div>
                         </div>
